dc183483c1d10b383de2fae6fd9a31bbaedcc7ba
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-11 19:25:18 -0300

    added views related to account app

--- ecommerce_game/accounts/views.py
+++ ecommerce_game/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from accounts.serializers import (
+    ChangePasswordSerializer,
+    RegisterSerializer,
+    UpdateUserSerializer,
+)
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
