21c6c849d4698c80d5c70a6bef920e3804f47d7c
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-11 19:25:49 -0300

    add init file with imports

---
+++ ecommerce_game/accounts/serializers/__init__.py
@@ -0,0 +1,9 @@
+from accounts.serializers.change_password_serializer import ChangePasswordSerializer
+from accounts.serializers.register_serializer import RegisterSerializer
+from accounts.serializers.update_user_serializer import UpdateUserSerializer
+
+__all__ = [
+    "ChangePasswordSerializer",
+    "RegisterSerializer",
+    "UpdateUserSerializer",
+]
