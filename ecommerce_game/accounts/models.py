1631cebf11a899f7ffafe255adb4bb78e3c0270d
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-11 17:34:01 -0300

    add usermanager  and user model

--- ecommerce_game/accounts/models.py
+++ ecommerce_game/accounts/models.py
@@ -1,3 +1,78 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class UserManager(BaseUserManager):
+    """UserManager class."""
+
+    def create_user(self, username, email, password):
+        """Create and return a User with an email, username and password."""
+        if not username:
+            raise TypeError("Users must have a username.")
+
+        if not email:
+            raise TypeError("Users must have an email address.")
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """Create and return a User with superuser (admin) permissions."""
+        if not password:
+            raise TypeError("Superusers must have a password.")
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    bio = models.TextField(null=True)
+    full_name = models.CharField(max_length=20000, null=True)
+    birth_date = models.DateField(null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        """Return a string representation of this User."""
+        string = self.email if self.email != "" else self.get_full_name()
+        return f"{self.id} {string}"
+
+    @property
+    def tokens(self):
+        """Allow to get a user's token by calling user.token."""
+        refresh = RefreshToken.for_user(self)
+        return {"refresh": str(refresh), "access": str(refresh.access_token)}
+
+    def get_full_name(self):
+        """Return the full name of the user."""
+        return self.full_name
+
+    def get_short_name(self):
+        """Return user username."""
+        return self.username
