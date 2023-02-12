727b94ce31293045cc7fb124c3fa8d4ea3d19554
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 04:06:16 -0300

    add product list view

--- backend/market/views.py
+++ backend/market/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from market.models import Product
+from market.serializers.product_serializer import ProductSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 
 # Create your views here.
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = ProductSerializer
