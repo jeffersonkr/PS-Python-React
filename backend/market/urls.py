366774ffd8633ee77afb3a28f0ecde7e36b85487
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 19:08:55 -0300

    add into url more path of cart and purchase

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from market.views import ProductListView, PurchaseView, CartView
+from market.views import ProductListView, PurchaseView, CartView, GetPurchaseView, CartCreateView
 
 urlpatterns = [
     path("product/", ProductListView.as_view(), name="product_list"),
     path("purchase/", PurchaseView.as_view(), name="create_purchase"),
-    path("cart/", CartView.as_view(), name="create_cart"),
+    path("purchase/<int:pk>", GetPurchaseView.as_view(), name="get_purchase"),
+    path("cart/", CartCreateView.as_view(), name="create_cart"),
+    path("cart/<int:pk>", CartView.as_view(), name="create_cart"),
 ]
