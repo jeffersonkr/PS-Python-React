947973cc86dc7b721a1c032f793c12854f3d488a
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 18:51:20 -0300

    add path for create purchase and cart

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from market.views import ProductListView
+from market.views import ProductListView, PurchaseView, CartView
 
 urlpatterns = [
     path("product/", ProductListView.as_view(), name="product_list"),
+    path("purchase/", PurchaseView.as_view(), name="create_purchase"),
+    path("cart/", CartView.as_view(), name="create_cart"),
 ]
