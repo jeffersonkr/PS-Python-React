0c869c126b3d0fdeb66051ac2ac6c17705102a06
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 18:51:31 -0300

    add view to purchase and cart

--- backend/market/views.py
+++ backend/market/views.py
@@ -1,7 +1,9 @@
-from market.models import Product
+from market.models import Product, Purchase, Cart
 from market.serializers.product_serializer import ProductSerializer
+from market.serializers.purchase_serializer import PurchaseSerializer
+from market.serializers.cart_serializer import CartSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 # Create your views here.
@@ -9,3 +11,15 @@ class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = ProductSerializer
+
+
+class PurchaseView(generics.CreateAPIView):
+    queryset = Purchase.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = PurchaseSerializer
+
+
+class CartView(generics.CreateAPIView):
+    queryset = Cart.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CartSerializer
