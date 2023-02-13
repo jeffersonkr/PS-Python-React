39c1773d8c233d817b4a72cabe12cda8c905ea97
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 21:24:21 -0300

    remove unused imports and add missging imports

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,10 +4,9 @@ from accounts.serializers import (
     UpdateUserSerializer,
 )
 from django.contrib.auth import get_user_model
-from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+
 
 User = get_user_model()
 
