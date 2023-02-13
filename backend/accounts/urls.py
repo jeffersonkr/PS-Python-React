44ea9ec822004d74be7846aa3b46b391adafeade
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 21:24:00 -0300

    renamed path name's

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from accounts.views import ChangePasswordView, RegisterView, UpdateProfileView
 from django.urls import path
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="auth_register"),
-    path("change_password/<uuid:pk>/", ChangePasswordView.as_view(), name="auth_change_password"),
-    path("update_profile/<uuid:pk>/", UpdateProfileView.as_view(), name="auth_update_profile"),
+    path("register/", RegisterView.as_view(), name="account_register"),
+    path("change_password/<uuid:pk>/", ChangePasswordView.as_view(), name="change_password"),
+    path("update_profile/<uuid:pk>/", UpdateProfileView.as_view(), name="update_profile"),
 ]
