3485279e1d02b685d9d4b48163e3adeba32d92e8
jefferson kwak <jeffersonkr@hotmail.com>
2023-02-12 22:48:30 -0300

    fix cart/pk path name

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("purchase/", PurchaseView.as_view(), name="create_purchase"),
     path("purchase/<int:pk>", GetPurchaseView.as_view(), name="get_purchase"),
     path("cart/", CartCreateView.as_view(), name="create_cart"),
-    path("cart/<int:pk>", CartView.as_view(), name="create_cart"),
+    path("cart/<int:pk>", CartView.as_view(), name="retrieve_update_cart"),
 ]
